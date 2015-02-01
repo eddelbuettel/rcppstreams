ee5fe6dc346268076c048ea4a352ef738062c5d6
Dirk Eddelbuettel <edd@debian.org>
2015-02-01 08:52:13 -0600

    new header file for RcppStreams

---
+++ inst/include/RcppStreams.h
@@ -0,0 +1,26 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+//
+//  RcppStreams -- Rcpp bindings to Streamulus for Event-based stream processing
+//
+//  Copyright (C) 2015           Dirk Eddelbuettel 
+//
+//  This file is part of RcppStreams
+//
+//  RcppStreams is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  RcppStreams is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with RcppStreams.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <Rcpp.h>
+
+## We define a StreamulusOutput object 
+#define StreamulusOut Rcpp::Rcout
+
